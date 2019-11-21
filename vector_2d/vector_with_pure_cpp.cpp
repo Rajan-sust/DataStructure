c3e1b9b9a9498147f855b443d58e4dd4eced7e09
Rajan-sust <imrajon0@gmail.com>
2019-11-21 11:14:21 +0600

    pure cpp implementation of vector

---
+++ vector_2d/vector_with_pure_cpp.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+template <typename T>
+class Vector {
+private:
+    T *container;
+    int size;
+public:
+    Vector() {
+        size = 0;
+        container = new T[1];
+    }
+    void push_back(T num) {
+        container[size] = num;
+        size += 1;
+        if((size & (size-1)) == 0) {
+            T *temp;
+            temp = new T[(size<<1)];
+            for(int i=0; i<size; i++){
+                temp[i] = container[i];
+            }
+            container = temp;
+        }
+    }
+    T operator [](int idx) {
+        return container[idx];
+    }
+    int length() {
+        return size;
+    }
+};
+
+
+
+int main(int argc, char const *argv[]) {
+    Vector<int>v;
+    for(int i=1; i<=10; i++) {
+        v.push_back((i*i));
+    }
+    for(int i=0;i<v.length(); i++) {
+        std::cout<<v[i]<<std::endl;
+    }
+    
+}
